9e83df2f2c4fde35071e7e6fea0f45ae3509bfe5
Katharine Rossinskaya <71548252+Techi77@users.noreply.github.com>
2022-02-07 11:45:19 +0300

    add: Adapter to CardsRecycler

--- app/src/main/java/com/example/androiddevelopertest/CardsRecycler/Adapter.kt
+++ app/src/main/java/com/example/androiddevelopertest/CardsRecycler/Adapter.kt
@@ -2,9 +2,12 @@ package com.example.androiddevelopertest.CardsRecycler
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androiddevelopertest.FragmentMain.Retrofit.Users
+import com.example.androiddevelopertest.R
 import com.example.androiddevelopertest.databinding.MyCardsElementBinding
 
 class Adapter(private val historyInfoList: Users):RecyclerView.Adapter<Adapter.MyViewHolder>() {
@@ -12,6 +15,8 @@ class Adapter(private val historyInfoList: Users):RecyclerView.Adapter<Adapter.M
     class MyViewHolder(binding: MyCardsElementBinding): RecyclerView.ViewHolder(binding.root){
 
         val cardNumber: TextView = binding.tvCustomCardNumber
+        val cardIcon: ImageView = binding.ivCustomCardIcon
+        val cardActiveStatus: ImageView = binding.customCardActiveStatus
 
     }
 
@@ -24,7 +29,16 @@ class Adapter(private val historyInfoList: Users):RecyclerView.Adapter<Adapter.M
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.cardNumber.text = historyInfoList.users[0].transaction_history[position].amount
+        holder.cardNumber.text = historyInfoList.users[position].card_number
+        holder.cardIcon.setImageResource(
+            when(historyInfoList.users[position].type){
+                "mastercard" -> R.drawable.ic_mastercard
+                "visa" -> R.drawable.ic_visa
+                "unionpay" -> R.drawable.ic_unionpay
+                else -> R.drawable.ic_custom_card_system
+            }
+        )
+        holder.cardActiveStatus.isVisible = position==0
     }
 
 }
--- app/src/main/java/com/example/androiddevelopertest/MyCardsScreen.kt
+++ app/src/main/java/com/example/androiddevelopertest/MyCardsScreen.kt
@@ -1,16 +1,56 @@
 package com.example.androiddevelopertest
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.androiddevelopertest.FragmentMain.Retrofit.Common
+import com.example.androiddevelopertest.FragmentMain.Retrofit.RetrofitServices
+import com.example.androiddevelopertest.FragmentMain.Retrofit.Users
+import com.example.androiddevelopertest.CardsRecycler.Adapter
 import com.example.androiddevelopertest.databinding.MyCardsBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyCardsScreen: AppCompatActivity() {
     private lateinit var binding: MyCardsBinding
+
+    lateinit var mService: RetrofitServices
+    lateinit var layoutManager: LinearLayoutManager
+    lateinit var adapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MyCardsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        mService = Common.retrofitService
+        layoutManager = LinearLayoutManager(this)
+
+        //History recycler
+        binding.recyclerViewHistory.setHasFixedSize(true)
+        binding.recyclerViewHistory.layoutManager = layoutManager
+
+        getHistoryList()
+
+        binding.btBack.setOnClickListener {
+            val intent = Intent(this@MyCardsScreen, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun getHistoryList() {
+        mService.getCardsData().enqueue(object : Callback<Users> {
+            override fun onFailure(call: Call<Users>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<Users>, response: Response<Users>) {
+                val cardBase = response.body() as Users
+                adapter = Adapter(cardBase)
+                binding.recyclerViewHistory.adapter = adapter
+            }
+        })
     }
 }
